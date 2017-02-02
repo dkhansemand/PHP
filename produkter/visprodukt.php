7fabe85f71f6f0fb392c46dcf4de4d80fbd898f3
root <root@5CG426FF3C.uv.rts.local>
2017-02-02 08:59:26 +0100

    Code commented and code structure is now readable and put in a table

--- produkter/visprodukt.php
+++ produkter/visprodukt.php
@@ -1,19 +1,21 @@
 <?php
 
+## Import class for mysql connection (via PDO)
 require_once './class.mysql.php';
 
+##Check if GET is available 
 if($_GET){
-    if(!empty($_GET["id"]) && is_numeric($_GET["id"])){
-        $conn = new dbconnector();
-        $query = $conn->newQuery("SELECT id, produktnavn, produktinfo, produktpris FROM produkter WHERE id = :ID");
-        $query->bindParam(":ID", $_GET["id"], PDO::PARAM_INT);
+    if(!empty($_GET["id"]) && gettype($_GET["id"]) === 'int'){ //check id ID is not empty and is type of INT
+        $conn = new dbconnector(); //Create an instance to connect
+        $query = $conn->newQuery("SELECT id, produktnavn, produktinfo, produktpris FROM produkter WHERE id = :ID");//Prepare SQL statement befor input
+        $query->bindParam(":ID", $_GET["id"], PDO::PARAM_INT);//Get input fomr URL and check param type is INT
         $query->execute();
-        $produkt = $query->fetch(PDO::FETCH_ASSOC);
+        $produkt = $query->fetch(PDO::FETCH_ASSOC); //Create array with row
     }else{
-        header('Location: ./visprodukter.php');
+        header('Location: ./visprodukter.php'); //IF id is null/empty set or type of INT - redirect
     }
 }else{
-    header('Location: ./visprodukter.php');
+    header('Location: ./visprodukter.php');//If GET is not set - redirect
 }
 ?>
 <!DOCTYPE html>
@@ -23,10 +25,23 @@ if($_GET){
     <title>PHP | Produkt </title>
 </head>
 <body>
-    <h1>Produkt: <?=$produkt["produktnavn"];?></h1>
-    <h2>Produkt info:</h2>
-    <p><?=$produkt["produktinfo"];?></p>
-    <h3>Produkt pris</h3>
-    <p><strong><?=$produkt["produktpris"];?></strong> kr.</p>
+
+    <table border=1>
+        <thead>
+            <tr>
+                <th>Produkt navn</th>
+                <th>Produkt info</th>
+                <th>Produkt pris</th>
+            </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td><?=$produkt["produktnavn"];?></td>
+            <td><?=$produkt["produktinfo"];?></td>
+            <td><?=$produkt["produktpris"];?></td>
+        </tr>
+        </tbody>
+    </table>
+    <a href="visprodukter.php">Tilbage</a>
 </body>
 </html>
